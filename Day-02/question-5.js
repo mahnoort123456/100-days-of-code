40f7ab28a09b622772fd50ff6506657602565755
Mahnoor <mahnoort104@gmail.com>
2024-09-02 00:44:45 +0500

    Add files via upload

---
+++ Day-02/question-5.js
@@ -0,0 +1,8 @@
+"use strict";
+/** Question 5
+Famous Quote 2: Repeat Exercise 4, but this time store
+ the famous person’s name in a variable called famous_person.
+ Then compose your message and store it in a new variable called message. Print your message. */
+let famous_person = "Winston S. Churchill once said,";
+let quote = "“Success is not final, failure is not fatal: It is the courage to continue that counts.”";
+console.log(`${famous_person}${quote}`);
---
+++ Day-02/question-6.js
@@ -0,0 +1,11 @@
+"use strict";
+/**Question 6
+Stripping Names: Store a person’s name, and include some whitespace characters
+ at the beginning and end of the name.
+ Make sure you use each character combination, " " and " ", at least once.
+  Print the name once, so the whitespace around the name is displayed.
+ Then print the name after striping the white spaces. */
+let whiteSpace = "\t Mahnoor \n";
+let withOutSpace = whiteSpace.trim();
+console.log(withOutSpace);
+console.log(whiteSpace);
