303ccd241ba7a4b062f1208efa602ca71e89cb60
Mahnoor <mahnoort104@gmail.com>
2024-09-03 01:02:44 +0500

    Add files via upload

---
+++ Day-03/question-9.js
@@ -0,0 +1,7 @@
+"use strict";
+/** Question 9
+Favorite Number: Store your favorite number in a variable. Then, using that variable,
+ create a message that reveals your favorite number. Print that message. */
+let favoriteNumber = 29;
+let message = "My favorite number is:";
+console.log(`${message} ${favoriteNumber}`);
