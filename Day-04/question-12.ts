76ecb54f2b3a967e769b2a0f3264fdbe6c40d3a3
Mahnoor <mahnoort104@gmail.com>
2024-09-03 23:50:10 +0500

    Add files via upload

---
+++ Day-04/question-12.ts
@@ -0,0 +1,11 @@
+/** Question 12
+Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
+print a message to them.
+The text of each message should be the same, but each message should be personalized with the person’s name. */
+
+let friendsName : string[] = ["Mahnoor Tahir" , "Nabiya Fahim" , "Anumta Nadeem" , "Syeda Esha" , "Sheeza Fatima"];
+console.log(`${friendsName[0]}, Do you like coffee?`);
+console.log(`${friendsName[1]}, Do you like coffee?`);
+console.log(`${friendsName[2]}, Do you like coffee?`);
+console.log(`${friendsName[3]}, Do you like coffee?`);
+console.log(`${friendsName[4]}, Do you like coffee?`);
