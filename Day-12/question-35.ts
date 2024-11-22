b013aad8625e478743d4b56ba2cadf16a6ba2a71
Mahnoor <mahnoort104@gmail.com>
2024-11-23 02:08:35 +0500

    Add files via upload

---
+++ Day-12/question-35.ts
@@ -0,0 +1,6 @@
+let animals: string[] = ["dog", "cat", "cow"];
+
+animals.forEach((animal) => {
+  console.log(`A ${animal} would make a great pet.`);
+});
+console.log("Any of these animals would make a great pet!");
